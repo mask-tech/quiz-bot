5ee5d129ed7dc2ee081f93fad48a9cefce61c05d
Vidunram A R <64352864+FriedPotato03@users.noreply.github.com>
2022-06-26 15:13:44 +0530

    Temp Commit 2
    Sending Questions in DM  and Quiz running facility initialised.

--- react_check.py
+++ react_check.py
@@ -1,4 +1,4 @@
-import discord
+import discord, json
 from asyncio import sleep
 from datetime import datetime
 from discord.ext import commands
@@ -8,21 +8,30 @@ chdir('\\'.join(__file__.split('\\')[:-1]))     #Changing path to keep files in 
 quizbot = commands.Bot(command_prefix = '<>', intents = discord.Intents.all(), help_command = None)
 quiz_progress = {}
 loaded_quizzes = {}
-# server_id : {'participants' : [#list of participants] , 'status' : 0}
+responses = {}
 
 with open('\\'.join(__file__.split('\\')[:-2])+'\\token.txt') as file: token = file.read()
 
+def format_question(guild_id: int, question: dict):
+    return f"**Quiz ID:** {guild_id} | {quiz_progress[guild_id]['quiz_id']}\n__**Sauce:**__ {question['sauce']}\n__**Text:**__ {question['text']}\n"+'\n'.join([f"{i} {question['options'][i]}" for i in question['options']])
+
 async def quiz_refresh(guild_id):
     '''Main quiz work is done here.'''
-    if status == 0:
+    for user in quiz_progress[guild_id]['participants']:
+        await user.send(f"You have been registered for **Quiz {quiz_progress[guild_id]['quiz_id']}** in server **{quizbot.get_guild(guild_id)}**")
+        with open(f'quizzes/{quiz_progress[guild_id]["quiz_id"]}.json', encoding = 'utf-16') as file:
+            loaded_quizzes[guild_id] = json.load(file)
+        await quiz_progress[guild_id]['channel'].send("**Participants of the quiz:\n**" + '\n'.join([f'<@{user.id}>' for user in quiz_progress[guild_id]['participants']])+"\nQuiz will start in 5 seconds.")
+        await sleep(5)
+    quiz_progress[guild_id]['status'] = 1
+    while quiz_progress[guild_id]['status'] <= loaded_quizzes[guild_id]['quiz_length']:
+        message_string = format_question(guild_id, loaded_quizzes[guild_id]['quiz'][quiz_progress[guild_id]['status']-1])
         for user in quiz_progress[guild_id]['participants']:
-            await user.send(f"You have been registered for **Quiz {quiz_progress[guild_id]['quiz_id']}** in server **{quizbot.get_guild(guild_id)}**")
-            with open(f'quizzes/{quiz_progress[guild_id]["quiz_id"]}.json') as file:
-                loaded_quizzes[guild_id] = json.load(file)
-            await sleep(5)
-            await quiz_progress[guild_id]['channel'].send("**Participants of the quiz:\n**" + '\n'.join([f'<@{user.id}>' for user in quiz_progress[message.guild.id]['participants']])+"\nQuiz will start in 5 seconds.")
-    status += 1    
-
+            await user.send(message_string)
+            await sleep(15)
+        quiz_progress[guild_id]['status'] += 1
+    else:
+        quiz_progress.pop(guild_id)
 
 @quizbot.event
 async def on_ready():
@@ -72,8 +81,11 @@ async def on_message(message):
             await message.add_reaction('🇾')
             await sleep(15)
             await message.delete()
-            quiz_refresh(message.guild.id)            
-
+            await quiz_refresh(message.guild.id)            
+        if message.content.startswith("**Quiz ID:**"):
+            guild_id = int(message.content.split('\n')[0].split()[2])
+            print(guild_id)
+            for i in loaded_quizzes[guild_id]['quiz'][quiz_progress[guild_id]['status']-1]['options']: await message.add_reaction(i)
 
 @quizbot.command(name = 'exit', aliases = ['kill', 'off', 'disconnect'])
 async def exit(ctx):
@@ -85,6 +97,10 @@ async def exit(ctx):
 @quizbot.command(name = 'start_quiz', aliases = ['start', 'begin', 'begin_quiz'])
 async def start_quiz(ctx, quiz_id: int):
     '''Starts the quiz. Afterwards, another async function will be used to run the quiz'''
+    try: open(f'quizzes/{quiz_id}.json', encoding = 'utf-16').close()
+    except:
+        await ctx.send("Quiz ID does not exist. Please check the quiz ID or contact Goos.")
+        return None
     if quiz_progress[ctx.message.guild.id]: 
         await ctx.send("A quiz is already registered in this server. ")
         return None

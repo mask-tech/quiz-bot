264e5c379dd85831449fd742b012ba67b588d6f2
Vidunram A R <64352864+FriedPotato03@users.noreply.github.com>
2022-06-26 12:04:31 +0530

    Commit 3.3
    Added refresh function for bot to check for quiz actions.
    Added constraint for reaction checking.

--- react_check.py
+++ react_check.py
@@ -7,8 +7,14 @@ from os import chdir
 chdir('\\'.join(__file__.split('\\')[:-1]))     #Changing path to keep files in same folder
 quizbot = commands.Bot(command_prefix = '<>', intents = discord.Intents.all(), help_command = None)
 quiz_progress = {}
-token = '... not posting it here lol'
 # server_id : {'participants' : [#list of participants] , 'status' : 0}
+
+with open('\\'.join(__file__.split('\\')[:-2])+'\\token.txt') as file: token = file.read()
+
+async def quiz_refresh(guild_id):
+    '''Main quiz work is done here.'''
+    for user in quiz_progress[guild_id]['participants']:
+        await user.send(f"You have been registered for **Quiz {quiz_progress[guild_id]['quiz_id']}** in server **{quizbot.get_guild(guild_id)}**")
 
 @quizbot.event
 async def on_ready():
@@ -17,6 +23,7 @@ async def on_ready():
         print(f"Up and running in server [{guild}]")
         try: open(f'logs/{guild.id}.txt', 'r+', encoding = 'utf-16').close()
         except IOError: open(f'logs/{guild.id}.txt', 'w', encoding = 'utf-16').close()
+        quiz_progress[guild.id] = None
 
 @quizbot.event
 async def on_reaction_add(reaction, user):
@@ -26,8 +33,8 @@ async def on_reaction_add(reaction, user):
             pass
         else:
             if reaction.message.content.startswith("**Starting Quiz ID:**"):
-                if user != quizbot.user and reaction.emoji == '🇾':
-                    quiz_progress[reaction.message.guild.id]['participants'].append(user.name)
+                if user != quizbot.user and reaction.emoji == '🇾' and quiz_progress[reaction.message.guild.id]['status']==0 and reaction.message.id == quiz_progress[reaction.message.guild.id]['message_id']:
+                    quiz_progress[reaction.message.guild.id]['participants'].append(user)
                     print(f"Participant added: {user.name}")
 
 @quizbot.event
@@ -38,8 +45,8 @@ async def on_reaction_remove(reaction, user):
             pass
         else:
             if reaction.message.content.startswith("**Starting Quiz ID:**"):
-                if user != quizbot.user and reaction.emoji == '🇾':
-                    quiz_progress[reaction.message.guild.id]['participants'].remove(user.name)
+                if user != quizbot.user and reaction.emoji == '🇾' and quiz_progress[reaction.message.guild.id]['status']==0 and reaction.message.id == quiz_progress[reaction.message.guild.id]['message_id']:
+                    quiz_progress[reaction.message.guild.id]['participants'].remove(user)
                     print(f"Participant removed: {user.name}")
 
 @quizbot.event
@@ -53,7 +60,13 @@ async def on_message(message):
         except discord.ext.commands.errors.CommandNotFound: message.channel.send("Command does not exist. :-!")
     elif message.author == quizbot.user:
         if message.content.startswith("**Starting Quiz ID:**"):
+            quiz_progress[message.guild.id]['message_id'] = message.id
             await message.add_reaction('🇾')
+            await sleep(15)
+            await message.delete()
+            await message.channel.send("**Participants of the quiz:\n**" + '\n'.join([f'<@{user.id}>' for user in quiz_progress[message.guild.id]['participants']])+"\nQuiz will start in 5 seconds.")
+            await sleep(5)
+            await quiz_refresh(message.guild.id)
 
 
 @quizbot.command(name = 'exit', aliases = ['kill', 'off', 'disconnect'])
@@ -66,8 +79,11 @@ async def exit(ctx):
 @quizbot.command(name = 'start_quiz', aliases = ['start', 'begin', 'begin_quiz'])
 async def start_quiz(ctx, quiz_id: int):
     '''Starts the quiz. Afterwards, another async function will be used to run the quiz'''
-    quiz_progress[ctx.message.guild.id] = {'participants' : [] , 'status' : 0}
-    await ctx.send(f"**Starting Quiz ID:** {quiz_id}. React with 🇾 to participate.")
-    await sleep(10)
+    if quiz_progress[ctx.message.guild.id]: 
+        await ctx.send("A quiz is already registered in this server. ")
+        return None
+    quiz_progress[ctx.message.guild.id] = {'quiz_id': quiz_id , 'participants' : [] , 'status' : 0}
+    await ctx.send(f"**Starting Quiz ID:** {quiz_id}. React with 🇾 to participate within 15 seconds.")
 
 quizbot.run(token)
+#Honk
